b11d004f1d7611f976cb1b24b783cb45d6defc88
tegner <jesper.h.tegner@jppol.dk>
2022-04-01 10:15:41 +0200

    Move consent naming to here (#3)

---
+++ src/getpurposetext.ts
@@ -0,0 +1,17 @@
+import type { TConsentTextKeys } from './types';
+import { CONSENTNAMES, CONSENTTEXTS } from './state';
+
+export function getPurposeText(consentTo: TConsentTextKeys) {
+  switch (consentTo) {
+    case CONSENTNAMES.fullconsent:
+      return CONSENTTEXTS.fullconsent;
+    case CONSENTNAMES.marketing:
+      return CONSENTTEXTS.marketing;
+    case CONSENTNAMES.preferences:
+      return CONSENTTEXTS.preferences;
+    case CONSENTNAMES.statistics:
+      return CONSENTTEXTS.statistics;
+    default:
+      return CONSENTTEXTS.fullconsent;
+  }
+}
