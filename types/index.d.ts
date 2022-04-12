c2562bab98e09850ebcd9936ca0e4fea9fee56cf
tegner <jesper.h.tegner@jppol.dk>
2022-04-12 15:17:24 +0200

    rebuild

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,6 +4,6 @@ export { getPurposeText } from './getpurposetext';
 export { noConsentGroup } from './getnoconsentgroup';
 export { CONSENTNAMES, CONSENTTEXTS } from './state';
 import type { TLoadStatus } from './types';
+export declare let consentID: string;
 export declare const editConsent: () => void;
-export declare const consentID: string;
 export declare let loadStatus: TLoadStatus;
