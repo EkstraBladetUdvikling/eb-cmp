bcac5b023bf4ab52c448bc6ea9f8bac21826c1ae
MathiasNordby <mathias_nordby@hotmail.com>
2022-05-25 10:13:25 +0200

    only index

--- src/gettcfdata.ts
+++
@@ -1,7 +0,0 @@
-import type { ITCData } from './types';
-
-export function getTCstring(): void {
-  window.__tcfapi('getTCData', 2, (tcData: ITCData) => {
-    return tcData.tcString;
-  });
-}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export { doWeHaveConsent } from './dowehaveconsent';
 export { getAllConsents } from './getallconsents';
 export { getPurposeText } from './getpurposetext';
 export { noConsentGroup } from './getnoconsentgroup';
-export { getTCstring } from './gettcfdata';
 export { CONSENTNAMES, CONSENTTEXTS } from './state';
 
 import { componentName } from './config';
